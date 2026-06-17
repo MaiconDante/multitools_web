dd8b430d22c02b17a5e4625d61edfd0336662119
MaiconDante <maicondante@hotmail.com>
2026-06-17 19:34:57 -0300

    feat: Cria interface da ferramenta RTF para DOCX.

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/rtf-docx")
+def rtf_docx():
+    return render_template("rtf_to_docx.html")
---
+++ app/services/rtf_to_docx.py
@@ -0,0 +1,5 @@
+def converter_rtf_para_docx(caminho_arquivo):
+    """
+    Implementação será feita na próxima etapa.
+    """
+    pass
