eb7a3f3d9c5c27f3a63185fe33db849b078bd4f8
MaiconDante <maicondante@hotmail.com>
2026-06-17 19:46:54 -0300

    feat: Implementa estrutura inicial de conversao RTF para DOCX.

--- app/services/rtf_to_docx.py
+++ app/services/rtf_to_docx.py
@@ -1,5 +1,19 @@
-def converter_rtf_para_docx(caminho_arquivo):
+from pathlib import Path
+
+from docx import Document
+
+
+def converter_rtf_para_docx(arquivo_rtf, arquivo_docx):
     """
-    Implementação será feita na próxima etapa.
+    Conversão simples de RTF para DOCX.
     """
-    pass
+
+    with open(arquivo_rtf, "r", encoding="utf-8", errors="ignore") as arquivo:
+        conteudo = arquivo.read()
+
+    documento = Document()
+    documento.add_paragraph(conteudo)
+
+    documento.save(arquivo_docx)
+
+    return arquivo_docx
